be5b93db3e312ecf5258f93ae770c39acfb5e735
André Medeiros <andre@setadesenvolvimentos.com.br>
2020-01-10 20:31:12 -0300

    Create signal events
    * New event_type_t: SIGNAL_EVENT
    * event.timer.repeating moved to the top level to represent recurring 
    signals
    * Immediate timers now are created with the due time set to the current 
    time (execution on the evloop will occur once evloop_run is called)
    * Removed old credit header

--- src/system/event.c
+++ src/system/event.c
@@ -2,8 +2,7 @@
 
 void event_destroy(event_t *event){
     switch (event->type){
-        case CLOSURE_EVENT:
-        case TIMER_EVENT:
+        default:
             closure_destroy(&event->closure);
             break;
     }
@@ -14,16 +13,25 @@ void event_config_closure(event_t *event, closure_t *closure){
     event->closure = *closure;
 }
 
+void event_config_signal(event_t *event, bool repeating, closure_t *closure){
+    event->type = SIGNAL_EVENT;
+    event->closure = *closure;
+    event->repeating = repeating;
+}
+
 void event_config_timer(
     event_t *event,
     uint16_t timeout_in_ms,
     bool repeating,
+    bool immediate,
     closure_t *closure,
     uint32_t current_time
 ){
     event->type = TIMER_EVENT;
-    event->timer.due_time = current_time + timeout_in_ms;
-    event->timer.timeout = timeout_in_ms;
-    event->timer.repeating = repeating;
     event->closure = *closure;
+    event->repeating = repeating;
+    event->timer.due_time = immediate ?
+        current_time :
+        current_time + timeout_in_ms;
+    event->timer.timeout = timeout_in_ms;
 }
--- src/system/event.h
+++ src/system/event.h
@@ -1,10 +1,3 @@
-/*
- * File:   event.h
- * Author: kazeshi
- *
- * Created on January 4, 2020, 2:44 PM
- */
-
 #ifndef EVENT_H
 #define	EVENT_H
 
@@ -14,7 +7,8 @@
 
 enum event_type {
     CLOSURE_EVENT,
-    TIMER_EVENT
+    TIMER_EVENT,
+    SIGNAL_EVENT
 };
 typedef enum event_type event_type_t;
 
@@ -22,20 +16,22 @@ typedef struct event event_t;
 struct event {
     event_type_t type;
     closure_t closure;
+    bool repeating;
     struct{
         uint32_t due_time;
         uint16_t timeout;
-        bool repeating;
     } timer;
 };
 
 void event_destroy(event_t *event);
 
 void event_config_closure(event_t *event, closure_t *closure);
+void event_config_signal(event_t *event, bool repeating, closure_t *closure);
 void event_config_timer(
     event_t *event,
     uint16_t timeout_in_ms,
     bool repeating,
+    bool immediate,
     closure_t *closure,
     uint32_t current_time
 );
