a92eea52a6f39bad1ba48504e383cdfdd4fee305
André Medeiros <andre@setadesenvolvimentos.com.br>
2020-01-15 16:48:49 -0300

    Doxygen comments

--- src/system/application.h
+++ src/system/application.h
@@ -1,3 +1,9 @@
+/**  \file application.h
+   * \brief Application module
+   *
+   * Contains the application_t struct as well as functions to manipulate it.
+   */
+
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
@@ -7,6 +13,7 @@
 #include "event-loop.h"
 #include "scheduler.h"
 #include "signal.h"
+#include "../utils/circular-queue.h"
 
 enum app_event{
     APP_READY = 0,
@@ -15,33 +22,70 @@ enum app_event{
 };
 typedef enum app_event app_event_t;
 
+//! The size of the event queue in log2 form
 #define APP_EVENT_QUEUE_SIZE_LOG2N (5)
 #define APP_EVENT_QUEUE_SIZE (1<<APP_EVENT_QUEUE_SIZE_LOG2N)
 
+//! The size of the reschedule queue in log2 form
 #define APP_RESCHEDULE_QUEUE_SIZE_LOG2N (4)
 #define APP_RESCHEDULE_QUEUE_SIZE (1<<APP_RESCHEDULE_QUEUE_SIZE_LOG2N)
 
+/** \brief Top-level container for µEvLoop'd application
+  *
+  * The application module is not necessary, but it does facilitate creating
+  * and managing the state of a µEvLoop application.
+  * It contains all the system insternals and manages the scheduler and event
+  * loops queues automatically.
+  */
 typedef struct application application_t;
 struct application{
-    pools_t pools;
+    pools_t pools; //!< Holds the system pools: events and llist nodes
 
-    evloop_t event_loop;
-    scheduler_t scheduler;
+    evloop_t event_loop; //!< The application's event loop
+    scheduler_t scheduler;  //!< The applications's scheduler;
 
-    signal_relay_t relay;
-    llist_t relay_buffer[APP_EVENT_COUNT];
+    signal_relay_t relay;   //!< Unused
+    llist_t relay_buffer[APP_EVENT_COUNT]; //!< Unused
 
+    //! \internal The event queue buffer
     void *event_queue_buffer[APP_EVENT_QUEUE_SIZE];
-    cqueue_t event_queue;
+    cqueue_t event_queue;   /**< \brief The application's event queue.
+                              *
+                              * Holds events ready to be processed on the next
+                              * runloop.
+                              */
 
+    //! \internal The reschedule queue buffer
     void *reschedule_queue_buffer[APP_RESCHEDULE_QUEUE_SIZE];
-    cqueue_t reschedule_queue;
+    cqueue_t reschedule_queue; /**< \brief The application's reschedule queue.
+                                 *
+                                 * Hold events already processed by the runloop
+                                 * but fit for rescheduling at the scheduler.
+                                 */
 
-    bool run_scheduler;
+    bool run_scheduler; //!< \internal Marks when it's time to wake the scheduler
 };
 
+/** \brief Initialises an application_t instance
+  * \param app The application_t instance
+  */
 void app_init(application_t *app);
+
+/** \brief Updates the internal timer of an application, located at the scheduler
+    \param app The application_t instance
+    \param timer The current application timer, in milliseconds
+  */
 void app_update_timer(application_t *app, uint32_t timer);
+
+/** \brief Ticks the application.
+  *
+  * Yields control to the application runtime. This will:
+  * 1. Check if the scheduler ought to be run (i.e.:  the counter has been
+  * updated or there are events awaiting rescheduling) and do so if necessary
+  * 2. Perform a runloop
+  *
+  * \param app The application_t instance
+  */
 void app_tick(application_t *app);
 
 #endif /* end of include guard: APPLICATION_H */
