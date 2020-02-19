0b25b7ed7c2cc22d11f1070aa2043f4a3ccc70aa
André Medeiros <andre@setadesenvolvimentos.com.br>
2020-02-19 10:46:56 -0300

    Automatically bumped version to 0.2.2

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['µevloop',['µEvLoop',['../index.html',1,'']]]
+  ['µevloop_20_21_5bc_2fc_2b_2b_20ci_5d_28https_3a_2f_2fgithub_2ecom_2fandsmedeiros_2fuevloop_2fworkflows_2fc_2fc_2b_2b_2520ci_2fbadge_2esvg_3fevent_3dpush_29',['µEvLoop ![C/C++ CI](https://github.com/andsmedeiros/uevloop/workflows/C/C++%20CI/badge.svg?event=push)',['../index.html',1,'']]]
 ];
